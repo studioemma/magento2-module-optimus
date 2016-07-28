6dbc31c1ccfa26e934323fbc93b4c4e2c45ffdef
Klaus De Buysser <klaus@studioemma.com>
2016-07-28 14:13:01 +0200

    MASE2OT-64 : added comment to old block PageWide about depecrated status

--- Block/PageWideContent.php
+++ Block/PageWideContent.php
@@ -1,5 +1,8 @@
 <?php
 
+
+/* Remark : this is now deprecated -> when using the most recent version of theme-frontend-optimus -> you can select a 1 'column wide' option on the Design tab of a cms page that does exactly the same */
+
 namespace StudioEmma\Optimus\Block;
 
 class PageWideContent extends \Magento\Framework\View\Element\Template
