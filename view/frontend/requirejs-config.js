75264d0a3bcb67f29402683c871c69b81f1ae364
Quinten Clause <quinten.clause@studioemma.eu>
2018-01-05 17:34:48 +0100

    Add cart open script
    when something is added to the cart
    
    Tickets: MASE2OT-87

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -1,5 +1,12 @@
 var config = {
     paths: {
         selectqty: 'StudioEmma_Optimus/js/selectqty',
+    },
+    config: {
+        mixins: {
+            'Magento_Checkout/js/view/minicart': {
+                'StudioEmma_Optimus/js/mixins/cart-open': true
+            }
+        }
     }
 };
---
+++ view/frontend/web/js/mixins/cart-open.js
@@ -0,0 +1,33 @@
+define(['jquery'], function ($) {
+    'use strict';
+
+    var minicart = $('[data-block="minicart"]');
+
+    var openTO = 0;
+
+    var updateCount = 0;
+
+    minicart.on('contentLoading', function () {
+        updateCount++;
+    });
+
+    var mixin = {
+
+        update: function (updatedCart) {
+
+            this._super(updatedCart);
+
+            if (updateCount >= 1) {
+                minicart.find('[data-role="dropdownDialog"]').dropdownDialog("open");
+                clearTimeout(openTO);
+                openTO = setTimeout(function() {
+                    minicart.find('[data-role="dropdownDialog"]').dropdownDialog("close");
+                }, 10000);
+            }
+        }
+    };
+
+    return function (target) {
+        return target.extend(mixin);
+    };
+});
