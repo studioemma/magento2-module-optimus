7d9ee6486b30f5c99f63010bd3becf79828157df
Quinten Clause <quinten.clause@studioemma.eu>
2018-01-08 12:38:34 +0100

    Refactor social media
    Tickets: MASE2OT-85

---
+++ Block/Html/Social.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace StudioEmma\Optimus\Block\Html;
+
+class Social extends \Magento\Framework\View\Element\Template
+{
+    protected $_scope = \Magento\Store\Model\ScopeInterface::SCOPE_STORE;
+
+    public function canRender()
+    {
+        return !empty($this->_scopeConfig->getValue('socialmediageneral/general/enablefollowlinks', $this->_scope));
+    }
+
+    public function getChannels()
+    {
+        return array('facebook' => 'Facebook', 'twitter' => 'Twitter', 'instagram' => 'Instagram', 'linkedin' => 'Linkedin', 'youtube' => 'Youtube', 'pinterest' => 'Pinterest', 'googleplus' => 'Google+', 'vimeo' => 'vimeo');
+    }
+
+    public function canRenderChannel($channel)
+    {
+        return !empty($this->_scopeConfig->getValue('socialmediachannels/'.$channel.'channel/enable'.$channel.'followlink', $this->_scope)) && !empty($this->_scopeConfig->getValue('socialmediachannels/'.$channel.'channel/'.$channel.'followlink', $this->_scope));
+    }
+
+    public function getChannelLink($channel)
+    {
+        return $this->_scopeConfig->getValue('socialmediachannels/'.$channel.'channel/'.$channel.'followlink', $this->_scope);
+    }
+
+    public function getChannelAlt($channel, $label)
+    {
+        return (!empty($this->_scopeConfig->getValue('socialmediachannels/'.$channel.'channel/'.$channel.'followlinkalt', $this->_scope))) ? $this->_scopeConfig->getValue('socialmediachannels/'.$channel.'channel/'.$channel.'followlinkalt', $this->_scope) : sprintf(__('Follow us on %s'), $label);
+    }
+
+    public function getChannelCssClass($channel)
+    {
+        return (!empty($this->_scopeConfig->getValue('socialmediachannels/'.$channel.'channel/'.$channel.'icon', $this->_scope))) ? 'social-custom' : 'social-'.$channel;
+    }
+
+    public function channelHasIcon($channel) {
+        return !empty($this->_scopeConfig->getValue('socialmediachannels/'.$channel.'channel/'.$channel.'icon', $this->_scope));
+    }
+
+    public function getChannelIcon($channel) {
+        return '/media/socialmedia/'.$this->_scopeConfig->getValue('socialmediachannels/'.$channel.'channel/'.$channel.'icon', $this->_scope);
+    }
+
+    public function getConfig()
+    {
+        return $this->_scopeConfig;
+    }
+}
