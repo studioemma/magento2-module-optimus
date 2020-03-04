1d111b81d04c54165f5d414b5e96f4a089ae3ea1
Quinten Clause <quinten.clause@studioemma.eu>
2020-03-04 11:46:10 +0100

    Fix layout of index pages
    (issues with mega menu and update code to more recent Magento stuff)

--- Controller/Index/Index.php
+++ Controller/Index/Index.php
@@ -1,45 +1,26 @@
 <?php
-/**
- * Copyright © 2015 Magento. All rights reserved.
- * See COPYING.txt for license details.
- */
 
 namespace StudioEmma\Optimus\Controller\Index;
 
-/**
- * Responsible for loading page content.
- *
- * This is a basic controller that only loads the corresponding layout file. It may duplicate other such
- * controllers, and thus it is considered tech debt. This code duplication will be resolved in future releases.
- */
 class Index extends \Magento\Framework\App\Action\Action
 {
-    /** @var \Magento\Framework\View\Page\Config */
-    private $pageConfig;
-
     /** @var \Magento\Framework\View\Result\PageFactory */
     private $pageFactory;
 
-
     public function __construct(
         \Magento\Framework\App\Action\Context $context,
-        \Magento\Framework\View\Page\Config $pageConfig,
         \Magento\Framework\View\Result\PageFactory $pageFactory
     ) {
         parent::__construct($context);
-        $this->pageConfig = $pageConfig;
         $this->pageFactory = $pageFactory;
     }
     /**
-     * Load the page defined in view/frontend/layout/samplenewpage_index_index.xml
-     *
      * @return \Magento\Framework\View\Result\Page
      */
     public function execute()
     {
-        $this->pageConfig->addBodyClass('page-content-examples');
-        $this->pageConfig->addBodyClass('cms-page-view');
-        $this->pageConfig->addBodyClass('cms-page');
-        return $this->pageFactory->create();
+        $resultPage = $this->pageFactory->create();
+        $resultPage->addHandle('content_default_update');
+        return $resultPage;
     }
 }
--- Controller/Interaction/Index.php
+++ Controller/Interaction/Index.php
@@ -1,15 +1,5 @@
 <?php
-/**
- * Copyright © 2015 Magento. All rights reserved.
- * See COPYING.txt for license details.
- */
 
 namespace StudioEmma\Optimus\Controller\Interaction;
 
-/**
- * Responsible for loading page content.
- *
- * This is a basic controller that only loads the corresponding layout file. It may duplicate other such
- * controllers, and thus it is considered tech debt. This code duplication will be resolved in future releases.
- */
 class Index extends \StudioEmma\Optimus\Controller\Index\Index { }
